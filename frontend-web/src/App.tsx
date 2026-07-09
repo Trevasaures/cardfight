5d903bb329731293e4a3c7688980d928e5cb9121
Trevor Colen <trevorcolen1@gmail.com>
2026-07-08 22:33:29 -0500

    Add nation quiz

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,13 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AppShell } from "./components/layout/AppShell";
-import { Analytics } from "./pages/Analytics";
-import { CardLibrary } from "./pages/CardLibrary";
 import { Dashboard } from "./pages/Dashboard";
-import { DeckBuilder } from "./pages/DeckBuilder";
-import { DeckLibrary } from "./pages/DeckLibrary";
-import { MatchHistory } from "./pages/MatchHistory";
 import { PlayLab } from "./pages/PlayLab";
+import { DeckLibrary } from "./pages/DeckLibrary";
+import { CardLibrary } from "./pages/CardLibrary";
+import { DeckBuilder } from "./pages/DeckBuilder";
+import { NationQuiz } from "./pages/NationQuiz";
+import { MatchHistory } from "./pages/MatchHistory";
+import { Analytics } from "./pages/Analytics";
 import { Rivalries } from "./pages/Rivalries";
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/decks" element={<DeckLibrary />} />
         <Route path="/cards" element={<CardLibrary />} />
         <Route path="/deck-builder" element={<DeckBuilder />} />
+        <Route path="/nation-quiz" element={<NationQuiz />} />
         <Route path="/matches" element={<MatchHistory />} />
         <Route path="/analytics" element={<Analytics />} />
         <Route path="/rivalries" element={<Rivalries />} />
